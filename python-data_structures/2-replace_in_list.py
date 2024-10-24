6a922bad9a30af437f667d24e502023542e8acdd
inyangelarissa <l.inyange@alustudent.com>
2024-10-24 06:54:35 -0700

    2-replace_in_list.py

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    boolist = my_list[:]
-    for count, i in enumerate(my_list):
-        if i % 2 == 0:
-            boolist[count] = True
-        else:
-            boolist[count] = False
-    return(boolist)
+def replace_in_list(my_list, idx, element):
+    if 0 <= idx < len(my_list):
+        my_list[idx] = element
+    return(my_list)
