c806d7106bcf214bdeefe6cc919245e0403b25b7
inyangelarissa <l.inyange@alustudent.com>
2025-01-31 09:15:36 -0800

    9-

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,5 +1,2 @@
 #!/usr/bin/node
-function add (a, b) {
-    console.log(a + b);
-  }
-  add(parseInt(process.argv[2]), parseInt(process.argv[3]));
+console.log(process.argv[2] + ' is ' + process.argv[3]);
